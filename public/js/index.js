57c5a7b1a24e5d933088f01a0f397c04c72732ef
Laura Miguel <miguellaura240@gmail.com>
2024-07-27 22:11:27 +0200

    changing marker color depending en paralympic or not

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 const dataUrl = "https://data.paris2024.org/api/explore/v2.1/catalog/datasets/paris-2024-sites-de-competition/records?limit=63"
 
+const redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+const blueIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 const readData = async () => {
     try {
         const res = await fetch(dataUrl, {
@@ -18,17 +36,18 @@ const readData = async () => {
 }
 
 async function createMap() {
-    let myMap = L.map("map").setView([46.71109, 1.7191036], 2);
+    let map = L.map("map").setView([46.71109, 1.7191036], 2);
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         // Attribution is obligatory as per copyright!
         maxZoom: 20
-    }).addTo(myMap);
+    }).addTo(map);
 
     let data = await readData();
     data.results.forEach((result) => {
-        L.marker([result.point_geo.lat, result.point_geo.lon]).addTo(myMap);
+        let color = (result.category_id === "venue-paralympic") ? redIcon : blueIcon;
+        L.marker([result.point_geo.lat, result.point_geo.lon], { icon: color }).addTo(map);
     });
 
 }

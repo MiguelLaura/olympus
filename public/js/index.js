cc90e67b11c83f4343f6b506cd82e1c425fa0b25
Laura Miguel <miguellaura240@gmail.com>
2024-07-27 21:57:52 +0200

    adding tahiti

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,7 @@ const readData = async () => {
 }
 
 async function createMap() {
-    let myMap = L.map("map").setView([46.71109, 1.7191036], 6);
+    let myMap = L.map("map").setView([46.71109, 1.7191036], 2);
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',

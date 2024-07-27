d1fdd16c7acf6952fbe48b8fe9a64aa3111fd539
Laura Miguel <miguellaura240@gmail.com>
2024-07-27 22:57:14 +0200

    slightly change map window

--- public/js/index.js
+++ public/js/index.js
@@ -56,7 +56,7 @@ function onMapClick(e) {
 
 // ------------------------------------
 
-var map = L.map("map").setView([46.71109, 1.7191036], 2);
+var map = L.map("map").setView([27.019416, -76.640625], 3);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',

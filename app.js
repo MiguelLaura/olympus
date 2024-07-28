8492973c5a7f433ae4c1341016086e5b03a8a139
Laura Miguel <miguellaura240@gmail.com>
2024-07-28 09:46:51 +0200

    clean app.js

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
-
-// const books = [];
 
 // Set EJS as view engine
 app.set("view engine", "ejs");
 
 // Middleware
 app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
 app.get("/", (req, res) => {
   res.render("index");
 });
-
-// app.post("/submit", (req, res) => {
-//   const book = req.body.book;
-//   if (book) {
-//     books.push(book);
-//     console.log(`Book submitted: ${book}`);
-//   }
-//   res.redirect("/");
-// });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
